dbb8a0949fd1619f381213d632698c479921b431
Johnnyr81 <emailjohn81@gmail.com>
2019-10-01 18:22:37 -0500

    initial update

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
-// Your code goes here
+//Mouseover
+const funbus = document.querySelector(".logo-heading");
+funbus.addEventListener("mouseover", () => {
+    funbus.style.color = "yellow";
+    }
+);
+
+//Keydown
+const wbody = document.querySelector("body");
+wbody.addEventListener("keydown", function(){
+    wbody.style.backgroundColor = "lightblue";
+    }
+);
+
+//Wheel
+const h2 = document.querySelectorAll("h2");
+h2.forEach(e=>{
+    e.addEventListener("wheel", ()=>{
+        e.style.color = "green";
+            }
+        ); 
+    }
+);
+
+    
